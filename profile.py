751e60ef5eb8b62ebf19feeee2a7da14a0d29875
Rana Aldahmi <ranaaldahmi@Ranas-MacBook-Pro.local>
2026-06-17 14:30:19 -0700

    week2 lab 2

---
+++ profile.py
@@ -0,0 +1,19 @@
+full_name = "Rana Aldahmi"
+
+nickname = "Raina"
+
+age = "17"
+
+used_python_before = True
+
+hobbies = ["baking", "shopping", "gaming"]
+
+favorites = {
+    "food" == "chicken sandwhich",
+    "hobby" == "baking"
+}
+
+print(full_name, age, used_python_before, hobbies, favorites)
+
+
+
---
+++ variable.py
@@ -0,0 +1,9 @@
+first_name = "Rana"
+Last_name = "Aldahmi"
+
+name = first_name + " " + Last_name
+
+#print(first_name)
+#print(last_name)
+print(name)
+print(first_name, last_name)
