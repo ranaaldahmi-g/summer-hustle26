2c63434e32b7e48f96f4a3392ebbf34b6678db42
ranaaldahmi-g <rana.aldahmi5@gmail.com>
2026-06-29 12:07:26 -0700

    Add files via upload

---
+++ lab3_rana.py
@@ -0,0 +1,41 @@
+# Rana Aldahmi | lab 3 | Intro to python
+
+# Ticket 1
+
+username = "Ranaaldahmi"
+
+# 11 characters
+
+# ticket 2
+print("First character of username: ", username[0])
+
+print("last character of username: ", username[-1])
+print("Last index of username: ", len(username) - 1)
+
+# i predict the first letter of the username will be R and the last letter will be i
+
+# the last index len(username) -1 is minus 1 because it starts counting at 0
+
+# ticket 3
+print("Welcome to loop, " + username)
+print(f"Welcome to loop, {username}!")
+
+# yes i beliebve they will be identical
+# none of the methods were easy for me
+
+#ticket 4
+
+username[0] = "X" 
+
+#username[0]
+.upper() = "X"
+
+#prediction: i think it will give me an error because strings are immutable
+#immutable means that the string cannot be changed after it is created. 
+
+#ticket 5
+feed = ["hey everyone", "my favorite color is purple", "The weather is nice today"]
+
+print(feed[0])
+
+print(feed[3])
